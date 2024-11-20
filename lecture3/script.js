4ddcfc60c8241042be2796234ff997f977ce2997
vikasverma-here <vikasverma995619@gmail.com>
2024-11-20 12:35:31 +0530

    done lecture 3

---
+++ lecture3/script.js
@@ -0,0 +1,16 @@
+let a = 4;
+let b=3;
+console.log(a+b)
+
+// golabal keyworkd give me the  window object 
+console.log(global)
+
+// yha pe mujhe empty object mulega agr code ko node js se run karenge age browser me karenge to  window object milega
+console.log(this)  
+
+// isliye globalThis aya 2020 me taki sare run time envirenment me support kare
+console.log(globalThis)
+
+// mathcing the both it gives true
+
+console.log(globalThis === global)
